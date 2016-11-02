5c5343046949a31764429ffc7236beaf11f2467a
manh <manh@localhost.localdomain>
2016-11-03 01:39:04 +0700

    add max, min, angle convert

--- include/math/math.h
+++ include/math/math.h
@@ -8,12 +8,17 @@
 #include <math/matrix3.h>
 #include <math/matrix4.h>
 #include <math/quaternion.h>
+#include <math.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(a,b) (((a)>(b))?(a):(b))
 
+#define DEG_TO_RAD(X) (X * M_PI / 180)
+#define RAD_TO_DEG(X) (X * 180 / M_PI)
 
 #ifdef __cplusplus
 }
