ebf7bea14d7fa518200b0fca673e3031c0c0e9ab
manh <HiguchiIchiyo@yahoo.com>
2016-11-03 21:33:28 +0700

    add clang/gnu lambda

--- include/mstr/lambda.h
+++ include/mstr/lambda.h
@@ -12,15 +12,36 @@ EXTEND_REF_FUNC(lambda_func,
   void(*to_guard)(void*, void*);
 );
 
+/*
+  Clang does not support nested function but has block.
+  Block acts "like function" but invoking block as function type
+  will generate segment fault !
+*/
+#if defined(__clang__)
+EXTEND_REF(lambda_ref, lambda_func,
+  void(^callback)();
+  m_list* guards;
+);
+#else //GNU
 EXTEND_REF(lambda_ref, lambda_func,
   void(*callback)();
   m_list* guards;
 );
+#endif
 
 lambda_ref* new_lambda(void* func, void* guards,...);
 
+#if defined(__clang__)
+
+#define SAFE_NEW_LAMBDA(return_type,body,list...) \
+  new_lambda(^body,list,0);
+
+#else //GNU
+
 #define SAFE_NEW_LAMBDA(return_type,body,list...) \
   new_lambda(({return_type __fn__ body __fn__;}),list,0);
+
+#endif
 
 #ifdef __cplusplus
 }
