f14c0be3e84ce7c38bae94d114255fd034c41378
manh <manh@localhost.localdomain>
2016-11-04 23:30:10 +0700

    refine inherit structure

--- include/graphic/node.h
+++ include/graphic/node.h
@@ -7,29 +7,65 @@ extern "C" {
 
 #include <math/math.h>
 #include <base/ref.h>
+#include <mstr/list.h>
 
-EXTEND_REF_FUNC(g_node_func,
-	void(*visit)(void*);
-	void(*draw)(void*);
-);
+#define NODE_FLAG_DIRTY_TRANSFORM 1
+
+#define EXTEND_NODE_FUNC(type,content) \
+	EXTEND_REF_FUNC(type, \
+		void(*visit)(void*, matrix4, int); \
+		void(*draw)(void*); \
+		void(*add_child)(void*,void*); \
+		content \
+	);
 
 /*
 	note :
 	- define parent by weak_ref to prevent circular dependency
+	e.g :
+	root<->parent<->node
+	if node holds a strong reference to parent then parent will
+	have lowest refcount equal 2, it may not be released normally:
+		phase 1
+			root : ref_count = 1 (hold by parent)
+			parent : ref_count = 2 (hold by parent and node)
+			node : ref_count = 1 (hold by parent)
+		phase 2 - root calls release root
+			root : ref_count = 0 : released
+			parent : ref_count = 1 : unreleased (memory leak)
+			node : ref_count = 1 : unreleased (memory leak)
 */
 #define EXTEND_NODE(struct_type, func, content) \
 	EXTEND_REF(struct_type, func, \
 		vector3 position; \
 		vector3 anchor; \
-		vector2 size; \
-		quaternion quat; \
+		vector3 size; \
+		vector3 scale; \
 		matrix4 model; \
+		quaternion quat; \
+		matrix4 render_model; \
 		weak_ref* parent; \
 		m_list* children; \
+		int transform_dirty; \
 		content \
 	);
 
+EXTEND_NODE_FUNC(g_node_func,);
 EXTEND_NODE(g_node, g_node_func,);
+
+/*
+interface for subclass
+*/
+void free_node(g_node* node);
+void visit_node(g_node* node, matrix4, int);
+void node_add_child(g_node*, g_node*);
+void init_node(g_node* node);
+
+#define INHERIT_NODE_FUNC \
+	BASE_REF_FUNC_INHERIT, \
+	.free = free_node, \
+	.visit = visit_node, \
+	.add_child = node_add_child
 
 g_node* node_new();
 
--- include/graphic/sprite2d.h
+++ include/graphic/sprite2d.h
@@ -2,14 +2,39 @@
 #define _M_SPRITE2D_H
 
 #include <graphic/node.h>
+#include <graphic/graphic.h>
+#include <graphic/texture.h>
+#include <graphic/shader.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-EXTEND_NODE(g_sprite2d, g_node_func,
-  int data;
+EXTEND_NODE_FUNC(g_sprite2d_func,
+  void(*set_shader)(void*,void*);
+  void(*set_texture)(void*,void*);
 );
+
+EXTEND_NODE(g_sprite2d, g_sprite2d_func,
+  GLuint vbo;
+  GLuint ebo;
+  GLuint vao;
+  g_shader* shader;
+  g_texture* texture;
+);
+
+void sprite2d_free(g_sprite2d* node);
+void sprite2d_set_shader(g_sprite2d* node, g_shader* shader);
+void sprite2d_set_texture(g_sprite2d* node, g_texture* texture);
+void sprite2d_draw(g_sprite2d* node);
+void init_sprite2d(g_sprite2d* node);
+
+#define INHERIT_SPRITE2D_FUNC \
+	INHERIT_NODE_FUNC, \
+	.free = sprite2d_free, \
+	.set_shader = sprite2d_set_shader, \
+  .set_texture = sprite2d_set_texture, \
+  .draw = sprite2d_draw
 
 g_node* sprite2d_new();
 
