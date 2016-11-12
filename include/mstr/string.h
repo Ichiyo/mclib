4d8ba9ecb14966cf514c54de738b6b8b362a1c0d
manh <manh@localhost.localdomain>
2016-11-13 04:25:01 +0700

    Add replace,casesensitive,clear,insert,cocontain

--- include/mstr/string.h
+++ include/mstr/string.h
@@ -7,9 +7,16 @@
 extern "C" {
 #endif
 
-EXTEND_REF_FUNC(m_string_func,
-	void(*cat_str)(void*, void*);
-	void(*cat_char)(void*, void*src);
+EXTEND_REF_FUNC(m_string_func, \
+	void(*cat_str)(void*, void*); \
+	void(*cat_char)(void*, void*src); \
+	void(*replace_str)(void*, void* before, void* after); \
+	void(*to_uppercase)(void*); \
+	void(*to_lowercase)(void*); \
+	void(*clear)(void*); \
+	void(*insert_after)(); \
+	int(*contain_char)(); \
+	int(*contain_str)(); \
 );
 
 EXTEND_REF(m_string, m_string_func,
@@ -18,6 +25,8 @@ EXTEND_REF(m_string, m_string_func,
 );
 
 m_string* new_string();
+m_string* new_string_from_char(char* in);
+m_string* new_string_from_str(m_string* in);
 
 #ifdef __cplusplus
 }
--- src/string.c
+++ src/string.c
@@ -1,6 +1,7 @@
 #include "mstr/string.h"
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -14,9 +15,10 @@ static void free_string(m_string* arg)
 
 static void mstr_mstr(m_string* des, m_string* src)
 {
-	des->content = realloc(des->content, (des->length + src->length) * sizeof(char));
+	des->content = realloc(des->content, (des->length + src->length + 1) * sizeof(char));
 	memcpy(des->content + des->length, src->content, src->length);
 	des->length = des->length + src->length;
+	des->content[des->length] = '\0';
 }
 
 static void mstr_char(m_string* des, char* src)
@@ -28,6 +30,214 @@ static void mstr_char(m_string* des, char* src)
 	des->content[des->length] = '\0';
 }
 
+static void mstr_replace_string(m_string* str, m_string* before, m_string* after)
+{
+	m_string* temp = new_string();
+
+	int start = -1;
+	int end = -2;
+	int count = 0;
+	int first_between = -1;
+	for(int i = 0; i < str->length; i++)
+	{
+		if(str->content[i] == before->content[count])
+		{
+			if(first_between == -1 && count > 0 && str->content[i] == before->content[0])
+			{
+				first_between = i;
+			}
+
+			count++;
+			if(count == before->length)
+			{
+				if(end >= start)
+				{
+					//copy str[start,end] to temp
+					int size = end - start + 1;
+					temp->content = realloc(temp->content, (temp->length + size + 1) * sizeof(char));
+					strncpy(temp->content + temp->length, str->content + start, size);
+					temp->length = temp->length + size;
+					temp->content[temp->length] = '\0';
+				}
+				// copy after to temp
+				mstr_mstr(temp, after);
+				first_between = -1;
+				start = i+1;
+				end = i;
+				count = 0;
+			}
+			else if(i == str->length - 1)
+			{
+				end = i;
+				if(start < 0) start = 0;
+			}
+		}
+		else
+		{
+			if(start < 0) start = 0;
+
+			count = 0;
+			if(first_between > 0)
+			{
+				i = first_between;
+				first_between = -1;
+				count++;
+				end = i - 1;
+			}
+			else
+			{
+				if(str->content[i] == before->content[count])
+				{
+					end = i - 1;
+					count++;
+				}
+				else
+				{
+					end = i;
+				}
+			}
+
+			if(i == str->length - 1) end = i;
+		}
+	}
+	if(end >= start)
+	{
+		int size = end - start + 1;
+		temp->content = realloc(temp->content, (temp->length + size + 1) * sizeof(char));
+		strncpy(temp->content + temp->length, str->content + start, size);
+		temp->length = temp->length + size;
+		temp->content[temp->length] = '\0';
+	}
+
+	str->length = 0;
+	free(str->content);
+	str->content = 0;
+	mstr_mstr(str, temp);
+	temp->func->release(temp);
+}
+
+static void mstr_to_uppercase(m_string* str)
+{
+	int i = 0;
+	while(str->content[i])
+	{
+		str->content[i] = toupper(str->content[i]);
+		i++;
+	}
+}
+
+static void mstr_to_lowercase(m_string* str)
+{
+	int i = 0;
+	while(str->content[i])
+	{
+		str->content[i] = tolower(str->content[i]);
+		i++;
+	}
+}
+
+static void mstr_clear(m_string* str)
+{
+	str->length = 0;
+	if(str->content) free(str->content);
+	str->content = 0;
+}
+
+static void mstr_insert_after(m_string* str, m_string* stencil, m_string* in)
+{
+	//dirty implementation , need improvement
+	m_string* buff = new_string_from_str(stencil);
+	buff->func->cat_str(buff, in);
+	mstr_replace_string(str, stencil, buff);
+	buff->func->release(buff);
+}
+
+static int mstr_contain_char(m_string* str, char* in)
+{
+	int in_length = strlen(in);
+
+	int count = 0;
+	int first_between = -1;
+	for(int i = 0; i < str->length; i++)
+	{
+		if(str->content[i] == in[count])
+		{
+			if(first_between == -1 && count > 0 && str->content[i] == in[0])
+			{
+				first_between = i;
+			}
+			count++;
+			if(count == in_length)
+			{
+				return 1;
+			}
+		}
+		else
+		{
+			if(i == str->length - 1) break;
+
+			count = 0;
+			if(first_between > 0)
+			{
+				i = first_between;
+				first_between = -1;
+				count++;
+			}
+			else
+			{
+				if(str->content[i] == in[count])
+				{
+					first_between = -1;
+					count++;
+				}
+			}
+		}
+	}
+	return 0;
+}
+
+static int mstr_contain_str(m_string* str, m_string* in)
+{
+	int count = 0;
+	int first_between = -1;
+	for(int i = 0; i < str->length; i++)
+	{
+		if(str->content[i] == in->content[count])
+		{
+			if(first_between == -1 && count > 0 && str->content[i] == in->content[0])
+			{
+				first_between = i;
+			}
+			count++;
+			if(count == in->length)
+			{
+				return 1;
+			}
+		}
+		else
+		{
+			if(i == str->length - 1) break;
+
+			count = 0;
+			if(first_between > 0)
+			{
+				i = first_between;
+				first_between = -1;
+				count++;
+			}
+			else
+			{
+				if(str->content[i] == in->content[count])
+				{
+					count++;
+				}
+			}
+		}
+	}
+
+	return 0;
+}
+
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wincompatible-pointer-types"
 
@@ -36,7 +246,14 @@ static m_string_func base_m_string_func =
 	BASE_REF_FUNC_INHERIT,
 	.free = free_string,
 	.cat_str = mstr_mstr,
-	.cat_char = mstr_char
+	.cat_char = mstr_char,
+	.replace_str = mstr_replace_string,
+	.to_uppercase = mstr_to_uppercase,
+	.to_lowercase = mstr_to_lowercase,
+	.clear = mstr_clear,
+	.insert_after = mstr_insert_after,
+	.contain_str = mstr_contain_str,
+	.contain_char = mstr_contain_char
 };
 
 m_string* new_string()
@@ -46,6 +263,21 @@ m_string* new_string()
 	str->func = &base_m_string_func;
 	return str;
 }
+
+m_string* new_string_from_char(char* in)
+{
+	m_string* ret = new_string();
+	mstr_char(ret, in);
+	return ret;
+}
+
+m_string* new_string_from_str(m_string* in)
+{
+	m_string* ret = new_string();
+	mstr_mstr(ret, in);
+	return ret;
+}
+
 #pragma GCC diagnostic pop
 
 #ifdef __cplusplus
